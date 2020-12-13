f121d3241b7802907c9d31af3b827ebc175af596
narumij <narumij@me.com>
2020-12-13 11:53:11 +0900

    refactoring

--- src/DFA/B.hs
+++ src/DFA/B.hs
@@ -8,6 +8,7 @@ module DFA.B (
 import qualified DFA
 import qualified DFA.A as A
 
+
 data Moves input epsilon' state
     = Input  [(input, state)]
     | Epsilon state
@@ -23,10 +24,12 @@ type Graph state state' input epsilon'
 type SelectEpsilon epsilon'
   = epsilon' -> Bool
 
+
 fromB :: SelectEpsilon epsilon'
     -> [Graph state state' input epsilon']
     -> Maybe [A.Graph state state' input]
 fromB f = mapM (graph f)
+
 
 graph :: SelectEpsilon epsilon'
     -> Graph state state' input epsilon'
@@ -34,17 +37,20 @@ graph :: SelectEpsilon epsilon'
 graph f (DFA.Node s ee) = fmap (DFA.Node s) (edges f ee)
 graph _ (DFA.Leaf s)    = Just (DFA.Leaf s)
 
+
 edges :: SelectEpsilon epsilon'
     -> Moves input epsilon' node
     -> Maybe (DFA.Move input node)
-edges _ (Input   ii)                      = Just (DFA.Input ii)
-edges _ (Epsilon  e)                      = Just (DFA.Eplision e)
-edges f (Epsilon' en) | hasEpsilon' f en  = Just (DFA.Eplision (epsilon' f en))
-                      | otherwise         = Nothing
+edges _ (Input   ii)                   = Just (DFA.Input ii)
+edges _ (Epsilon  e)                   = Just (DFA.Eplision e)
+edges f (Epsilon' en) | isUnique f en  = Just (DFA.Eplision (epsilon f en))
+                      | otherwise      = Nothing
 
-epsilon' :: (a -> Bool) -> [(a, c)] -> c
-epsilon' f = snd . head . filter (f . fst)
 
-hasEpsilon' :: (b1 -> Bool) -> [(b1, b2)] -> Bool
-hasEpsilon' f = (==1) . length . filter (f . fst)
+epsilon :: (a -> Bool) -> [(a, c)] -> c
+epsilon f = snd . head . filter (f . fst)
+
+
+isUnique :: (b1 -> Bool) -> [(b1, b2)] -> Bool
+isUnique f = (==1) . length . filter (f . fst)
 
